a1994c44041d05ed2ad1c63c9b392cf4536ecc5a
gs647 <gs647@drexel.edu>
2022-02-20 17:25:50 -0500

    fixed usps

--- usps.js
+++ usps.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-const track = async(tracking_num) => {
+const track = async(tracking_num)=> {
   const usps_tracking_site = 'https://tools.usps.com/go/TrackConfirmAction_input';
   const tracking_search_bar = "#tracking-input";
   const search_track_button = '#trackPackage > div.col-sm-2.track-btn-ctn > button';
@@ -11,8 +11,18 @@ const track = async(tracking_num) => {
   //const estimated_date = '#st_App_PkgStsTimeDayMonthNum';
   //const package_status = '#stApp_txtPackageStatus';
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless : true});
   const page = await browser.newPage();
+  
+  await page.setExtraHTTPHeaders({
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.0 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+  });
+
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.0 Safari/537.36');
 
   await page.goto(usps_tracking_site);
 
@@ -21,6 +31,10 @@ const track = async(tracking_num) => {
   await page.type(tracking_search_bar, tracking_num);
 
   await page.click(search_track_button);
+
+  await page.screenshot({ path: 'example.png' });
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: 'example2.png' });
 
   await page.waitForSelector(class_day);
 
